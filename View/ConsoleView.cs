c12314c806daf3de9798842d06d17edb3bb3d295
MlodyRzodkiewa <szymon0418@gmail.com>
2021-10-06 19:37:24 +0200

    Implement ConsoleView

--- View/ConsoleView.cs
+++ View/ConsoleView.cs
@@ -1,8 +1,29 @@
-﻿using System;
+﻿using Gladiator.Model.Gladiators;
+using System;
+using System.Collections.Generic;
 
 namespace Gladiator.View
 {
     public class ConsoleView
     {
+        public List<string> Log { get; set; }
+
+        public void Display()
+        {
+            foreach (string line in Log)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public void DisplayWinMessage(BaseGladiator winner)
+        {
+            Console.WriteLine($"{winner.Name} wins!!!");
+        }
+
+        public int GetNumberBetween()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
