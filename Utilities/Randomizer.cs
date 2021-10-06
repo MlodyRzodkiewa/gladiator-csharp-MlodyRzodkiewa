8fcb7d5e47e292ce0b11358eb66a09fe8a99fc17
MlodyRzodkiewa <szymon0418@gmail.com>
2021-10-06 18:01:09 +0200

    Create and Implement Randomizer

---
+++ Utilities/Randomizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gladiator.Utilities
+{
+    class Randomizer
+    {
+        private static readonly Random _random = new Random();
+
+        public static int Get(int ceiling)
+        {
+            return _random.Next(ceiling);
+        }
+        public static int Get(int floor, int ceiling)
+        {
+            return _random.Next(floor, ceiling);
+        }
+
+    }
+}
