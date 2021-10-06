fdc79d3ae380654c74f0aaa7bf73462dee46f2cf
MlodyRzodkiewa <szymon0418@gmail.com>
2021-10-06 18:03:32 +0200

    Create MultiplierEnum.cs

---
+++ Utilities/MultiplierEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Gladiator.Model.Gladiators.BaseGladiator;
+
+namespace Gladiator.Utilities
+{
+    public static class MultiplierEnum
+    {
+        public static double GetMultiplier(this Multiplier multiplier) => multiplier switch
+        {
+            Multiplier.Low => 1,
+            Multiplier.Medium => 1.25,
+            Multiplier.High => 1.45,
+            _ => throw new ArgumentOutOfRangeException(),
+        };
+    }
+}
